e401c6617210b667f193b15a5688a999df092bf2
Yan Ferreira <fabioyan2@gmail.com>
2024-03-26 11:18:08 -0300

    chore: add react-query setup

--- projeto/src/App.tsx
+++ projeto/src/App.tsx
@@ -1,8 +1,13 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+
+export const queryClient = new QueryClient();
+
 function App() {
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h2 className="text-sm bg-red-700">Vite</h2>
-    </>
+    </QueryClientProvider>
   );
 }
 
