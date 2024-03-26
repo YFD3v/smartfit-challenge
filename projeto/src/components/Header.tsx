b1b323d0fa2ab8b86a9bc3b9b1acce7f8c819c1a
Yan Ferreira <fabioyan2@gmail.com>
2024-03-26 11:52:08 -0300

    refactor: esnure Header component uses  header html tag

--- projeto/src/components/Header.tsx
+++ projeto/src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div className="bg-black flex items-center justify-center p-5">
+    <header className="bg-black flex items-center justify-center p-5">
       <img src="/logo.svg" alt="Logo" width={180} />
-    </div>
+    </header>
   );
 };
 
