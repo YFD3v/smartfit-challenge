cdbcc44a2451966df339347c75a67a92b9242973
Yan Ferreira <fabioyan2@gmail.com>
2024-03-26 11:32:32 -0300

    chore: add brand colors

--- projeto/tailwind.config.js
+++ projeto/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand:{
+          darkGrey: "#333333",
+        lightGrey: "#808080",
+        yellow: "#FFB612",
+        red: "#dc0a17",
+        green: "#2FC022",
+        }
+      },
+    },
   },
   plugins: [],
-}
-
+};
